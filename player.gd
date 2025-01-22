0abb4e144f3ffebb39d363c35ee4b464bed17ceb
Jakub Sygnowski <sygnowski@gmail.com>
2025-01-22 18:27:22 -0500

    add all animation to the player

--- player.gd
+++ player.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @export var speed = 400 # px/s
 
+const POSSIBLE_DIRECTIONS = [&"right", &"left", &"down", &"up"]
+var last_direction: String = ""
 var screen_size: Vector2
 
 func _ready() -> void:
@@ -10,11 +12,23 @@ func _ready() -> void:
 
 	screen_size = get_viewport_rect().size
 
+func update_animation():
+	for direction in POSSIBLE_DIRECTIONS:
+		if Input.is_action_pressed(direction):
+			last_direction = direction
+			$AnimatedSprite2D.play(direction)
+			return
+
+	# Not moving right now.
+	if last_direction != "":
+		$AnimatedSprite2D.play(last_direction + "_idle")
+
 
 func _process(delta: float) -> void:
+	update_animation()
 	var direction = get_direction()
 	var velocity = direction * speed
-	
+
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 
@@ -29,5 +43,5 @@ func get_direction():
 		direction.y += 1
 	if Input.is_action_pressed(&"up"):
 		direction.y -= 1
-		
+
 	return direction.normalized()
