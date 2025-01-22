823fe96538c1de391cc9d9fd2ca8b8b64215fb2b
Keenan <keenancroucher@gmail.com>
2025-01-22 16:41:37 -0500

    move player with input

--- player.gd
+++ player.gd
@@ -1,12 +1,33 @@
 extends Node2D
 
+@export var speed = 400 # px/s
+
+var screen_size: Vector2
 
 func _ready() -> void:
 	# playing animation
 	$AnimatedSprite2D.play("going_up")
-	pass # Replace with function body.
+
+	screen_size = get_viewport_rect().size
 
 
 func _process(delta: float) -> void:
-	# TODO: fill by Keenan
-	pass
+	var direction = get_direction()
+	var velocity = direction * speed
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+
+func get_direction():
+	var direction = Vector2.ZERO
+	if Input.is_action_pressed(&"right"):
+		direction.x += 1
+	if Input.is_action_pressed(&"left"):
+		direction.x -= 1
+	if Input.is_action_pressed(&"down"):
+		direction.y += 1
+	if Input.is_action_pressed(&"up"):
+		direction.y -= 1
+		
+	return direction.normalized()
