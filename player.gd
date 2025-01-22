0e27943dd561b9e3059fe6cb3e6e62656135acce
Jakub Sygnowski <sygnowski@gmail.com>
2025-01-22 16:34:40 -0500

    turn on the animation

--- player.gd
+++ player.gd
@@ -2,9 +2,11 @@ extends Node2D
 
 
 func _ready() -> void:
+	# playing animation
+	$AnimatedSprite2D.play("going_up")
 	pass # Replace with function body.
 
 
 func _process(delta: float) -> void:
-  # TODO: fill by Keenan
+	# TODO: fill by Keenan
 	pass
