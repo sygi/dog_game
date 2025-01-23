d3a36e12797729ad09af9269df34883071a7cf33
Keenan <keenancroucher@gmail.com>
2025-01-23 12:28:43 -0500

    prototype of dog pulling, dogs = 1 for now

--- player.gd
+++ player.gd
@@ -1,12 +1,20 @@
 extends Node2D
 
+const POSSIBLE_DIRECTIONS = [&"right", &"left", &"down", &"up"]
+const NEXT_PULL_TIME = 0.5 # s
+const NEXT_PULL_RANGE = PI / 4 # rads
+const DOG_SPEED = 100 # px/s
+
 @export var speed = 400 # px/s
 @export var ACTIVATION_DISTANCE = 100 # px
 
-const POSSIBLE_DIRECTIONS = [&"right", &"left", &"down", &"up"]
 var last_direction: String = ""
 var screen_size: Vector2
 var active_dog = null
+
+var dogs: int = 1
+var pull_direction: Vector2
+var pull_timer: float = 0
 
 
 func get_closest_dog() -> Array:
@@ -47,9 +55,6 @@ func sprite_size() -> Vector2:
 	var as2d_size     = texture_size * $AnimatedSprite2D.get_scale()
 	return as2d_size
 
-func _ready() -> void:
-	screen_size = get_viewport_rect().size - sprite_size()
-
 func update_animation():
 	for direction in POSSIBLE_DIRECTIONS:
 		if Input.is_action_pressed(direction):
@@ -60,17 +65,22 @@ func update_animation():
 	# Not moving right now.
 	if last_direction != "":
 		$AnimatedSprite2D.play(last_direction + "_idle")
-
-
-func _process(delta: float) -> void:
-	update_animation()
-	activate_dog()
+		
+func move_player(delta: float):
 	var direction = get_direction()
 	var velocity = direction * speed
+	
+	pull_timer += delta
+	if (pull_timer > NEXT_PULL_TIME):
+		var pull_direction_change = randf_range(-NEXT_PULL_RANGE, NEXT_PULL_RANGE)
+		pull_direction = pull_direction.rotated(pull_direction_change)
+	pull_timer = fmod(pull_timer, NEXT_PULL_TIME)
+	
+	var pull_velocity = pull_direction * dogs * DOG_SPEED
+	velocity += pull_velocity
 
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
-
 
 func get_direction():
 	var direction = Vector2.ZERO
@@ -84,3 +94,13 @@ func get_direction():
 		direction.y -= 1
 
 	return direction.normalized()
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size - sprite_size()
+	
+	pull_direction = Vector2.UP.rotated(randf_range(0, TAU))
+
+func _process(delta: float) -> void:
+	update_animation()
+	activate_dog()
+	move_player(delta)
