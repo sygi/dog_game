c784a02073973cc25e3b7d2281212731f3e77767
Jakub Sygnowski <sygnowski@gmail.com>
2025-01-22 19:28:18 -0500

    activate the dog that is the closest to the player

--- dog.gd
+++ dog.gd
@@ -2,10 +2,23 @@ extends Node2D
 
 const BREEDS: Array = ["black", "white", "brown", "yellow"]
 @export var breed: String = BREEDS[randi() % BREEDS.size()]
+var is_active: bool = false
 
 func _ready() -> void:
 	$AnimatedSprite2D.play("idle_" + breed)
 
+
+func activate() -> void:
+	"""I'm close to the player"""
+	print("activating")
+	# TODO: how do you add some picture to show one gets activated?
+	draw_circle(Vector2.ZERO, 100, Color.WHITE)
+	is_active = true
+
+func deactivate() -> void:
+	"""I'm far from the player"""
+	print("deactivating")
+	is_active = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- player.gd
+++ player.gd
@@ -1,10 +1,44 @@
 extends Node2D
 
 @export var speed = 400 # px/s
+@export var ACTIVATION_DISTANCE = 100 # px
 
 const POSSIBLE_DIRECTIONS = [&"right", &"left", &"down", &"up"]
 var last_direction: String = ""
 var screen_size: Vector2
+var active_dog = null
+
+
+func get_closest_dog() -> Array:
+	var closest_dog = null
+	var closest_distance = INF
+	for dog in get_tree().get_nodes_in_group("dogs"):
+		var distance = position.distance_to(dog.position)
+		if distance < closest_distance:
+			closest_dog = dog
+			closest_distance = distance
+
+	if active_dog != null and closest_dog != active_dog:
+		active_dog.deactivate()
+		active_dog = null
+	return [closest_dog, closest_distance]
+
+func activate_dog():
+	# Do we have tuple unpacking here?
+	var dog_and_dist = get_closest_dog()
+	var dog = dog_and_dist[0]
+	if dog == null:
+		return
+	var distance = dog_and_dist[1]
+	if distance > ACTIVATION_DISTANCE:
+		if active_dog != null:
+			active_dog.deactivate()
+			active_dog = null
+		return
+
+	if not dog.is_active:
+		dog.activate()
+		active_dog = dog
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
@@ -23,6 +57,7 @@ func update_animation():
 
 func _process(delta: float) -> void:
 	update_animation()
+	activate_dog()
 	var direction = get_direction()
 	var velocity = direction * speed
 
