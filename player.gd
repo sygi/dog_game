204dc35694e75cdda2deedee670974666a199055
Jakub Sygnowski <sygnowski@gmail.com>
2025-01-24 11:33:29 -0500

    display all the dogs separately

--- player.gd
+++ player.gd
@@ -12,42 +12,48 @@ const DOG_SPEED = 100 # px/s
 var last_direction: String = ""
 var screen_size: Vector2
 var active_dog = null # TODO: remove this
-var leashed_dog = null
+var leashed_dogs = []
 
 var n_dogs: int = 0
 var dog_score: int = 0
 # We store current and the next pull direction
-var pull_directions_schedule: Array[Vector2] = [Vector2.UP.rotated(randf_range(0, TAU))]
+var pull_directions_schedule = []  # [dog, directions]
 var current_pull_direction: Vector2
 var pull_timer: float = 0
 var dog_scene = preload("res://dog.tscn")
 
 func set_dogs(value: int):
+	# This only does extra operations for dropping the number of dogs, we should fix it.
 	n_dogs = value
-	if n_dogs == 0 and leashed_dog != null:
-		leashed_dog.queue_free()
+	while leashed_dogs.size() > n_dogs:
+		leashed_dogs[-1].queue_free()
+		leashed_dogs.pop_back()
+	while pull_directions_schedule.size() > n_dogs:
+		pull_directions_schedule.pop_back()
 
 func get_closest_dog() -> Array:
 	var closest_dog = null
 	var closest_distance = INF
+	var closest_direction = null
 	for dog in get_tree().get_nodes_in_group("dogs"):
 		var distance = position.distance_to(dog.position)
 		if distance < closest_distance:
 			closest_dog = dog
 			closest_distance = distance
+			closest_direction = position.direction_to(dog.position)
 
 	if active_dog != null and closest_dog != active_dog:
 		active_dog.deactivate()
 		active_dog = null
-	return [closest_dog, closest_distance]
+	return [closest_dog, closest_distance, closest_direction]
 
 func activate_dog():
 	# Do we have tuple unpacking here?
-	var dog_and_dist = get_closest_dog()
-	var dog = dog_and_dist[0]
+	var dog_and_dist_dir = get_closest_dog()
+	var dog = dog_and_dist_dir[0]
 	if dog == null:
 		return
-	var distance = dog_and_dist[1]
+	var distance = dog_and_dist_dir[1]
 	if distance > ACTIVATION_DISTANCE:
 		if active_dog != null:
 			active_dog.deactivate()
@@ -58,7 +64,7 @@ func activate_dog():
 		var breed = dog.breed
 		dog.activate()
 		#active_dog = dog
-		add_leashed_dog(breed)
+		add_leashed_dog(breed, dog_and_dist_dir[2])
 
 func potentially_release_dog():
 	if Input.is_action_just_pressed(&"select"):
@@ -82,6 +88,12 @@ func update_animation():
 	# Not moving right now.
 	if last_direction != "":
 		$AnimatedSprite2D.play(last_direction + "_idle")
+		
+static func sum_array(array):
+	var sum = Vector2(0.0, 0.0)
+	for element in array:
+		sum += element
+	return sum
 
 func move_player(delta: float):
 	var direction = get_direction()
@@ -89,30 +101,33 @@ func move_player(delta: float):
 
 	pull_timer += delta
 	if (pull_timer > NEXT_PULL_TIME):
-		var pull_direction_change = randf_range(-NEXT_PULL_RANGE, NEXT_PULL_RANGE)
-		pull_directions_schedule.append(pull_directions_schedule[-1].rotated(pull_direction_change))
-	if pull_directions_schedule.size() > 2:
-		pull_directions_schedule.pop_front()
+		for dog in range(pull_directions_schedule.size()):
+			var pull_direction_change = randf_range(-NEXT_PULL_RANGE, NEXT_PULL_RANGE)
+			pull_directions_schedule[dog].append(pull_directions_schedule[dog][-1].rotated(pull_direction_change))
+			if pull_directions_schedule[dog].size() > 2:
+				pull_directions_schedule[dog].pop_front()
 	pull_timer = fmod(pull_timer, NEXT_PULL_TIME)
 	var proportion = pull_timer / NEXT_PULL_TIME
-	current_pull_direction = pull_directions_schedule[0].lerp(pull_directions_schedule[-1], proportion)
-	var pull_velocity = current_pull_direction * n_dogs * DOG_SPEED
+	var pull_directions = []
+	for dog in range(pull_directions_schedule.size()):
+		pull_directions.append(pull_directions_schedule[dog][0].lerp(pull_directions_schedule[dog][-1], proportion))
+	var pull_velocity = sum_array(pull_directions) * DOG_SPEED
 	velocity += pull_velocity
 
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
-	if leashed_dog != null:
-		# TODO: make this smoother
-		leashed_dog.position = current_pull_direction * LEASH_LENGTH
+	for dog in range(leashed_dogs.size()):
+		leashed_dogs[dog].position = pull_directions[dog] * LEASH_LENGTH
 
-func add_leashed_dog(breed: String):
-	set_dogs(n_dogs + 1)
-	if n_dogs == 1:
-		# first dog, visualizing it
-		leashed_dog = dog_scene.instantiate()
-		leashed_dog.breed = breed
-		leashed_dog.remove_from_group("dogs")
-		add_child(leashed_dog)
+func add_leashed_dog(breed: String, direction: Vector2):
+	set_dogs(n_dogs + 1)	
+	var leashed_dog = dog_scene.instantiate()
+	leashed_dog.breed = breed
+	leashed_dog.remove_from_group("dogs")
+	leashed_dog.position = direction * LEASH_LENGTH
+	add_child(leashed_dog)
+	leashed_dogs.append(leashed_dog)
+	pull_directions_schedule.append([direction, direction])
 
 func get_direction():
 	var direction = Vector2.ZERO
@@ -142,5 +157,3 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		dog_score += new_score
 		$"../Score".text = str(dog_score)
 		set_dogs(0)
-		if leashed_dog != null:
-			leashed_dog.queue_free()
