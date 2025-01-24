143b493ec70f06cefde2626536379194f8ce0a35
Jakub Sygnowski <sygnowski@gmail.com>
2025-01-24 15:50:37 -0500

    show leashes

--- dog.gd
+++ dog.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 const BREEDS: Array = ["black", "white", "brown", "yellow"]
 @export var breed: String = BREEDS[randi() % BREEDS.size()]
+@export var leash_color: Color = Color(0., 0.38, 0., 0.9)
 var running: bool = false
 var barking_sounds = [
 	preload("res://assets/dog_sounds/Dog/Dog Bark 0.wav"),
@@ -24,6 +25,13 @@ func start_running():
 	running = true
 	remove_from_group("dogs")
 
+func _draw():
+	if not running:
+		return
+	var player_offset = Vector2(80, 80)
+	var dog_offset = Vector2(sprite_size().x / 2, sprite_size().y / 2)
+	draw_line(dog_offset, -position + player_offset, leash_color, 10)
+
 func barking_delay():
 	return randf_range(0.7, 3.)
 
@@ -42,4 +50,6 @@ func _on_timer_timeout() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if running:
+		queue_redraw()
 	pass
