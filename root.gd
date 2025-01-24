9894fdcc4ae14ebccec61298656214514ceea5f6
Jakub Sygnowski <sygnowski@gmail.com>
2025-01-24 11:38:21 -0500

    only allow spawning dogs in some distance to the house and player

--- root.gd
+++ root.gd
@@ -3,6 +3,7 @@ extends Node2D
 const MAX_DOGS_ON_SCREEN = 3
 
 var dog_scene = preload("res://dog.tscn")
+@export var MIN_PLAYER_DIST = 100
 
 
 func fill_world_with_dogs():
@@ -10,12 +11,18 @@ func fill_world_with_dogs():
 	for i in range(MAX_DOGS_ON_SCREEN - existing_dogs):
 		var dog = dog_scene.instantiate()
 		var dog_size = dog.sprite_size()
-		dog.position = Vector2(
+		var preliminary_pos = Vector2(
 			randf_range(0, get_viewport().size.x - dog_size.x),
 			randf_range(0, get_viewport().size.y - dog_size.y)
 		)
+		while min(preliminary_pos.distance_to($Player.position), preliminary_pos.distance_to($House.position)) < MIN_PLAYER_DIST:
+			preliminary_pos = Vector2(
+				randf_range(0, get_viewport().size.x - dog_size.x),
+				randf_range(0, get_viewport().size.y - dog_size.y)
+			)
+		dog.position = preliminary_pos
 		$Dogs.add_child(dog)
-	
+
 func _ready() -> void:
 	fill_world_with_dogs()
 
